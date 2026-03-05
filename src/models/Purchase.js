0591f9a93aee4a0b9207c1f735c0b20ecf9d2f36
JetProc <soonjae8297@gmail.com>
2026-03-05 11:14:39 +0900

    feat: 로또 발행·구매 관련 모델/서비스 구현
    파일: src/services/LottoMachine.js, src/models/LottoResult.js, src/models/Purchase.js

---
+++ src/models/Purchase.js
@@ -0,0 +1,39 @@
+import { LOTTO_COST } from "../constants/config.js";
+import {
+  isInputEmpty,
+  isPositiveInteger,
+} from "../validates/CommonValidator.js";
+import { isValidUnit, isTooLarge } from "../validates/PurchaseValidator.js";
+
+class Purchase {
+  #money;
+
+  constructor(input) {
+    this.#validate(input);
+    this.#money = Number(input);
+  }
+
+  #validate(input) {
+    if (isInputEmpty(input)) {
+      throw new Error("[ERROR]");
+    }
+
+    if (!isPositiveInteger(input)) {
+      throw new Error("[ERROR]");
+    }
+
+    if (!isValidUnit(input)) {
+      throw new Error("[ERROR]");
+    }
+
+    if (isTooLarge(input)) {
+      throw new Error("[ERROR]");
+    }
+  }
+
+  // buyLottos() {
+  // return this.#money / LOTTO_COST;
+  // }
+}
+
+export default Purchase;
---
+++ src/services/LottoMachine.js
@@ -0,0 +1,15 @@
+class LottoMachine {
+  #lottoBundles;
+
+  //   constructor(lottoCount) {
+  //카운트를 로또를 발행해서 내부 변수에 저장함
+  // this.#makeLottos(lottoCount);
+  //   }
+
+  //   #makeLottos(lottoCount) {
+  //lottoCount만큼 랜덤 로또 2차원 배열을 this.#lottoBundles에 저장
+  // this.#lottoBundles = ~~
+  //   }
+}
+
+export default LottoMachine;
