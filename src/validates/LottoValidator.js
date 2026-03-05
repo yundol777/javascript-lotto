f06af61fac11951bed2870a866af87a9ecc3bbf0
JetProc <soonjae8297@gmail.com>
2026-03-05 18:11:25 +0900

    feat: 로또 검증 로직 보완
    파일: src/validates/LottoValidator.js

--- src/validates/LottoValidator.js
+++ src/validates/LottoValidator.js
@@ -1,5 +1,21 @@
+export function commonValidate() {
+  const isInputEmpty = input === "";
+  const isPositiveInteger = /^[1-9]\d*$/.test(input);
+  const isNumberInRange = Number(input) >= 1 && Number(input) <= 45;
+
+  return isNumberInRange && !isInputEmpty && isPositiveInteger;
+}
+
 export function isNumberInRange(input) {
   return Number(input) >= 1 && Number(input) <= 45;
+}
+
+export function isInputEmpty(input) {
+  return input === "";
+}
+
+export function isPositiveInteger(input) {
+  return /^[1-9]\d*$/.test(input);
 }
 
 export function isArrayInLength(array, length) {
@@ -13,4 +29,8 @@ export function isArrayUnique(array) {
 
 export function isNumberInArray(number, array) {
   return array.includes(number);
+}
+
+export function isWinningNumbersExist(winningLottos) {
+  return winningLottos;
 }
