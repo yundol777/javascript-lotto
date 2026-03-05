ff1c0ba0e1e1e4d995fb3f717eac0fba634f9819
JetProc <soonjae8297@gmail.com>
2026-03-05 18:11:25 +0900

    feat: 메인 컨트롤러 실행 흐름 개선
    파일: src/controllers/MainController.js

--- src/controllers/MainController.js
+++ src/controllers/MainController.js
@@ -3,13 +3,15 @@ import InputView from "../views/InputView.js";
 import Purchase from "../models/Purchase.js";
 import LottoMachine from "../services/LottoMachine.js";
 import OutputView from "../views/OutputView.js";
-import LottoResult from "../models/LottoResult.js";
+import WinningLotto from "../models/WinningLotto.js";
+import { INPUT_MESSAGE } from "../constants/message.js";
 
 class MainController {
   async run() {
     //1. 구입 금액 입력 받기
-    const purchaseInput =
-      await InputView.readStringWithMsg("> 구입금액을 입력해 주세요.");
+    const purchaseInput = await InputView.readStringWithMsg(
+      INPUT_MESSAGE.PURCHASE,
+    );
 
     //1-1. 구매 인스턴스 생성
     const purchase = new Purchase(purchaseInput);
@@ -23,16 +25,17 @@ class MainController {
     OutputView.printPurchasedLottos(lottoCount, lottoBundle);
 
     //3. 당첨 번호 입력 받기
-    const lottoResult = new LottoResult();
+    const winningLotto = new WinningLotto();
 
-    const winningLottosInput =
-      await InputView.readStringWithMsg("> 당첨 번호를 입력해 주세요. ");
+    const winningLottosInput = await InputView.readStringWithMsg(
+      INPUT_MESSAGE.WINNING_NUMBERS,
+    );
     //당첨 번호 검증 및 설정
-    lottoResult.setWinningNumbers(winningLottosInput);
+    winningLotto.setWinningNumbers(winningLottosInput);
 
     //4. 보너스 번호 입력 받기
     const bonusNumberInput = await InputView.readStringWithMsg(
-      "> 보너스 번호를 입력해 주세요. ",
+      INPUT_MESSAGE.BONUS_NUMBER,
     );
     //보너스 번호 검증 및 설정
     lottoResult.setBonusNumber(bonusNumberInput);
@@ -44,30 +47,3 @@ class MainController {
 }
 
 export default MainController;
-
-/*
-> 구입금액을 입력해 주세요.8000
-8개를 구매했습니다.
-[8, 21, 23, 41, 42, 43] 
-[3, 5, 11, 16, 32, 38] 
-[7, 11, 16, 35, 36, 44] 
-[1, 8, 11, 31, 41, 42] 
-[13, 14, 16, 38, 42, 45] 
-[7, 11, 30, 40, 42, 43] 
-[2, 13, 22, 32, 38, 45] 
-[1, 3, 5, 14, 22, 45]
-
-> 당첨 번호를 입력해 주세요. 1,2,3,4,5,6
-
-> 보너스 번호를 입력해 주세요. 7
-
-당첨 통계
---------------------
-3개 일치 (5,000원) - 1개
-4개 일치 (50,000원) - 0개
-5개 일치 (1,500,000원) - 0개
-5개 일치, 보너스 볼 일치 (30,000,000원) - 0개
-6개 일치 (2,000,000,000원) - 0개
-총 수익률은 62.5%입니다.
-
-> 다시 시작하시겠습니까? (y/n) */
