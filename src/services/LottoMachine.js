cd851e24ef41c8c27e0a1828678db59a0f1e99e4
JetProc <soonjae8297@gmail.com>
2026-03-05 17:07:12 +0900

    feat: 로또 발행 서비스 구현
    파일: src/services/LottoMachine.js

--- src/services/LottoMachine.js
+++ src/services/LottoMachine.js
@@ -1,15 +1,30 @@
+import { Random } from "@woowacourse/mission-utils";
+import Lotto from "../models/Lotto.js";
+
 class LottoMachine {
-  #lottoBundles;
+  #generatedLottos;
 
-  //   constructor(lottoCount) {
-  //카운트를 로또를 발행해서 내부 변수에 저장함
-  // this.#makeLottos(lottoCount);
-  //   }
+  constructor(lottoCount) {
+    this.#generatedLottos = this.#generateLottos(lottoCount);
+  }
 
-  //   #makeLottos(lottoCount) {
-  //lottoCount만큼 랜덤 로또 2차원 배열을 this.#lottoBundles에 저장
-  // this.#lottoBundles = ~~
-  //   }
+  getLottos() {
+    const lottos = this.#generatedLottos.map((lotto) => {
+      return lotto.getNumbers();
+    });
+
+    return lottos;
+  }
+
+  #generateLottos(lottoCount) {
+    const lottos = [];
+    for (let _ = 0; _ < lottoCount; _++) {
+      const numbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+      lottos.push(new Lotto(numbers));
+    }
+
+    return lottos;
+  }
 }
 
 export default LottoMachine;
