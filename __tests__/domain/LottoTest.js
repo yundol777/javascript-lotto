49af1e2cb04e8a2a7c4000318ec01eece2294245
JetProc <soonjae8297@gmail.com>
2026-03-05 17:02:26 +0900

    test: 테스트 구조 추가 및 기본 테스트 추가
    파일: __tests__/*

--- __tests__/domain/LottoTest.js
+++ __tests__/domain/LottoTest.js
@@ -1,3 +1,5 @@
+import Lotto from "../../src/models/Lotto.js";
+
 describe("로또와 당첨 번호를 비교한다.", () => {
   test("6개 번호 일치 1등", () => {});
   test("5개 번호 + 보너스 번호 일치 2등", () => {});
@@ -12,5 +14,8 @@ describe("로또와 당첨 번호를 비교한다.", () => {
 describe("로또 번호가 오름차순으로 정렬된다.", () => {
   test("로또 번호가 오름차순으로 정렬된다.", () => {
     //출력: 오름차순으로 정렬된 로또 번호 6개
+    const UNSORTED_ARRAY = [8, 3, 5, 1, 6, 2];
+    const SORTED_ARRAY = [1, 2, 3, 5, 6, 8];
+    expect(new Lotto(UNSORTED_ARRAY).getNumbers()).toEqual(SORTED_ARRAY);
   });
 });
