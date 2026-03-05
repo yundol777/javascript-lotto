72e7fef70f8ababf392eff3a5e11b495b2d02956
JetProc <soonjae8297@gmail.com>
2026-03-05 11:14:39 +0900

    test: 테스트 구조(도메인/검증/통합) 정리
    파일: __tests__

---
+++ __tests__/domain/LottoMachineTest.js
@@ -0,0 +1,14 @@
+import Purchase from "../../src/models/Purchase";
+import LottoMachine from "../../src/services/LottoMachine";
+
+describe("구입한 금액 만큼 로또가 발행된다.", () => {
+  test("구입한 금액 만큼 로또가 발행된다.", () => {
+    // const INPUT = 5000;
+    // 1. purchase 인스턴스를 만든다.
+    // const purchase = new Purchase(INPUT);
+    // 2. purchase 인스턴스 안의 구입한 금액을 로또 갯수로 바꿔주는 함수로 로또 갯수를 저장한다.
+    // const lottoCount = purchase.buyLottos();
+    // 3. lottoMachine 인스턴스에 구입한 개수를 넣어서 인스턴스를 만든다.
+    // const lottoMochine = new LottoMachine(lottoCount);
+  });
+});
---
+++ __tests__/validation/LottoTest.js
@@ -0,0 +1,51 @@
+import LottoResult from "../../src/models/LottoResult";
+
+describe("당첨 로또 번호 입력와 보너스 번호 입력에 공통적으로 적용되는 검증", () => {
+  // input: ""
+  test("숫자는 비어있을 수 없습니다.", () => {
+    const number = "";
+    const result = new LottoResult();
+    expect(() => result.setWinningNumbers(number)).toThrow("[Error]");
+  });
+
+  // input: a, -1, 0.1
+  test("숫자는 자연수여야 합니다.", () => {
+    const number = "10";
+    const result = new LottoResult();
+    expect(() => result.setWinningNumbers(number)).toThrow("[Error]");
+  });
+
+  // input: 58, 100
+  test("숫자는 1부터 45까지여야 합니다.", () => {
+    const number = "58";
+    const result = new LottoResult();
+    expect(() => result.setWinningNumbers(number)).toThrow("[Error]");
+  });
+});
+
+describe("당첨 로또 번호 입력 시 적용되는 검증", () => {
+  // input: [1,2,3,4,5], [1,2,3,4,5,6,7]
+  test("배열 안의 값은 6개여야 합니다.", () => {
+    const numbers = "1,2,3,4,5";
+    const result = new LottoResult();
+    expect(() => result.setWinningNumbers(numbers)).toThrow("[Error]");
+  });
+
+  // input: [1,1,2,3,4,5]
+  test("배열 안의 값은 중복되어서는 안됩니다.", () => {
+    const numbers = "1,1,2,3,4,5";
+    const result = new LottoResult();
+    expect(() => result.setWinningNumbers(numbers)).toThrow("[Error]");
+  });
+});
+
+describe("보너스 번호 입력 시 적용되는 검증", () => {
+  // input: 1 / [1,2,3,4,5,6]
+  test("보너스 번호는 당첨 로또 번호와 중복될 수 없습니다.", () => {
+    const result = new LottoResult();
+    const winningNumbers = "1,2,3,4,5,6";
+    result.setWinningNumbers(winningNumbers);
+    const bonusNumber = "1";
+    expect(() => result.setBonusNumber(bonusNumber)).toThrow("[Error]");
+  });
+});
