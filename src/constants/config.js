8f5d8872139bfc18bd2ae7e789875bb61ab5925a
yundoll <mar0722@naver.com>
2026-03-06 16:28:35 +0900

    feat: 수익률 계산 및 당첨금 정보 (LOTTO_RANK) 추가

--- src/constants/config.js
+++ src/constants/config.js
@@ -6,3 +6,31 @@ export const RETRY_ANSWER = {
   YES: "y",
   NO: "n",
 };
+
+export const LOTTO_RANK = {
+  "5th": {
+    matchCount: 3,
+    money: 5000,
+    requireBonus: false,
+  },
+  "4th": {
+    matchCount: 4,
+    money: 50000,
+    requireBonus: false,
+  },
+  "3rd": {
+    matchCount: 5,
+    money: 1500000,
+    requireBonus: false,
+  },
+  "2nd": {
+    matchCount: 5,
+    money: 30000000,
+    requireBonus: true,
+  },
+  "1st": {
+    matchCount: 6,
+    money: 2000000000,
+    requireBonus: false,
+  },
+};
--- src/services/LottoResult.js
+++ src/services/LottoResult.js
@@ -1,18 +1,27 @@
+import { resultAggregator } from "./ResultAggregator.js";
+import { calculateProfitRate } from "./CalculateProfitRate.js";
+import { LOTTO_COST } from "../constants/config.js";
+
 class LottoResult {
   #lottoResult;
 
-  constructor(winningLotto, lottoBundle) {
-    this.#calculateResult(winningLotto, lottoBundle);
+  constructor(winningLottoManager, lottoBundle) {
+    this.#calculateResult(winningLottoManager, lottoBundle);
   }
 
-  #calculateResult(winningLotto, lottoBundle) {
+  #calculateResult(winningLottoManager, lottoBundle) {
     this.#lottoResult = lottoBundle.map((lotto) => {
-      return winningLotto.compareWithWinningLotto(lotto);
+      return winningLottoManager.compareWithWinningLotto(lotto);
     });
   }
 
-  getResult() {
-    return this.#lottoResult;
+  // 당첨 통계와 수익률을 계산하여 반환 만약 나눈다면... 수익률 계산 함수의 위치가 조금 모호하다고 생각했음
+  getResult(lottoCount) {
+    // 테스트를 위해 순수 함수로 분리. (matchCount: 6, hasBonus: false) 만으로 테스트 가능하도록
+    const resultData = resultAggregator(this.#lottoResult);
+    const profitRate = calculateProfitRate(resultData, lottoCount * LOTTO_COST);
+
+    return { resultData, profitRate };
   }
 }
 
