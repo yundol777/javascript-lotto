605f1b1d0d370277b7346a289683f3707d8ecb44
yundoll <mar0722@naver.com>
2026-03-06 16:28:38 +0900

    feat: 로또 번호 및 당첨 통계 출력 구현

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,43 +1,43 @@
 import { Console } from "@woowacourse/mission-utils";
-// import { OUTPUT_MESSAGE } from "../constants/messages.js";
-// import { LOTTO_NUMBER_SEPARATOR } from "../constants/config.js";
-// import { formatNumber } from "../utils/Utils.js";
+import { OUTPUT_MESSAGE } from "../constants/message.js";
+import { formatNumber } from "../utils/FormatNumber.js";
+
 const OutputView = {
-  printPurchasedLottos(lottoCount, lottoNumberArrays) {
-    Console.print(`${lottoCount}개를 구매했습니다.`);
+  printPurchasedLottoTickets(lottoCount, lottoNumberArrays) {
+    Console.print(OUTPUT_MESSAGE.PURCHASE_COUNT(lottoCount));
 
     lottoNumberArrays.forEach((numbers) => {
-      Console.print(`[${numbers.join(`, `)}]`);
+      Console.print(OUTPUT_MESSAGE.LOTTO_NUMBERS(numbers));
     });
   },
 
-  // printResult(resultData, profitRate) {
-  //   Console.print(OUTPUT_MESSAGE.WINNING_STATISTICS_HEADER);
+  printResult(resultData, profitRate) {
+    Console.print(OUTPUT_MESSAGE.WINNING_STATISTICS_HEADER);
 
-  //   this.printWinningInfo(resultData);
+    this.printWinningInfo(resultData);
 
-  //   const formattedProfitRate = formatNumber(profitRate);
+    const formattedProfitRate = profitRate;
 
-  //   Console.print(OUTPUT_MESSAGE.PROFIT_RATE(formattedProfitRate));
-  // },
+    Console.print(OUTPUT_MESSAGE.PROFIT_RATE(formattedProfitRate));
+  },
 
-  // printWinningInfo(resultData) {
-  //   resultData.forEach((rank) => {
-  //     const { matchCount, money, requireBonus, count } = rank;
+  printWinningInfo(resultData) {
+    resultData.forEach((rank) => {
+      const { matchCount, money, requireBonus, count } = rank;
 
-  //     let matchMessage = OUTPUT_MESSAGE.WINNING_MATCH_COUNT_INFO(matchCount);
-  //     if (requireBonus) matchMessage += ", 보너스 볼 일치";
+      let matchMessage = OUTPUT_MESSAGE.WINNING_MATCH_COUNT_INFO(matchCount);
+      if (requireBonus) matchMessage += ", 보너스 볼 일치";
 
-  //     const formattedMoney = formatNumber(money);
-  //     const prizeMessage = OUTPUT_MESSAGE.WINNING_PRIZE_MONEY(formattedMoney);
+      const formattedMoney = formatNumber(money);
+      const prizeMessage = OUTPUT_MESSAGE.WINNING_PRIZE_MONEY(formattedMoney);
 
-  //     Console.print(`${matchMessage} ${prizeMessage} - ${count}개`);
-  //   });
-  // },
+      Console.print(`${matchMessage} ${prizeMessage} - ${count}개`);
+    });
+  },
 
-  // printMessage(message) {
-  //   Console.print(message);
-  // },
+  printMessage(message) {
+    Console.print(message);
+  },
 };
 
 export default OutputView;
